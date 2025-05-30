fc3de5dbdcfed7c23624b9e180dd9276fedeb2f0
Sefanovskis-Artjoms <artjoms200@gmail.com>
2025-05-30 08:19:24 +0300

    Setting up base with, for now non functional, components

--- FE/src/app/todos/todo-edit/todo-edit.ts
+++ FE/src/app/todos/todo-edit/todo-edit.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-todo-edit',
-  imports: [],
+  imports: [
+    RouterModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
   templateUrl: './todo-edit.html',
-  styleUrl: './todo-edit.scss'
+  styleUrl: './todo-edit.scss',
 })
 export class TodoEdit {
-
+  todo = {
+    id: 1,
+    title: 'Implement new feature',
+    description:
+      'This feature will enhance user experience by providing additional functionality.',
+    type: 'Feature',
+    status: 'Todo',
+    createdOn: new Date('2025-05-26'),
+  };
 }
