ebab00838ad5e6f6808c54f143052641aaf20acd
Sefanovskis-Artjoms <artjoms200@gmail.com>
2025-07-01 17:06:26 +0300

    design rework

--- FE/src/environments/environment.development.ts
+++ FE/src/environments/environment.development.ts
@@ -3,8 +3,8 @@ import { todo } from 'node:test';
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api',
-  todoApiUrl: 'http://localhost:8080/api/todos',
+  // todoApiUrl: 'http://localhost:8080/api/todos',
   userApiUrl: 'http://localhost:8080/api/users',
   // apiUrl: 'http://localhost:3002/api',
-  // todoApiUrl: 'http://localhost:3002/api/todos',
+  todoApiUrl: 'http://localhost:3002/api/todos',
 };
